706122dd5103a8c03e55af9dbb53658fd1480e9e
jkarmowska <julia.karmowska@gmail.com>
2022-05-15 12:49:51 +0200

    simple server

---
+++ chat_app/src/main.rs
@@ -0,0 +1,40 @@
+use tokio::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel(10); //sending strings to max 10 ppl who can connect
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();//klonowanie tx dla kazdego klienta
+        let mut rx = tx.subscribe();//nowy rx dla każdego klienta
+
+
+        tokio::spawn(async move { //spawnowanie taska obsługi klienta
+            let (reader, mut writer) = socket.split(); //podział socketa na czytanie i pisanie
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line( & mut line) => { //pierwszy branch - otrzymanie wiadomosci do przeslania
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => { //drugi branch - otrzymanie wiadomosci przez broadcast
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr{
+                        writer.write_all(msg.as_bytes()).await.unwrap();}
+                    }
+                }
+            }
+        });
+    }
+}
