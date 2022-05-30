87e53a09bc26f35f99d8f4dde337433b8dbd2c8a
jkarmowska <julia.karmowska@gmail.com>
2022-05-30 15:24:59 +0200

    add client

--- chat_app/src/bin/client.rs
+++ chat_app/src/bin/client.rs
@@ -1,3 +1,88 @@
-fn main() {
+use std::io::stdin;
+use std::net::SocketAddr;
+use std::sync::mpsc::Receiver;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
+use tokio::net::{TcpStream, tcp::ReadHalf};
+use tokio::net::tcp::WriteHalf;
+use tracing::{error, info};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::error::SendError;
 
+const SERVER: &str = "localhost:8080";
+const QUIT: &str = "/quit\n";
+
+
+//async fn receive(mut reader: BufReader<ReadHalf>, receiver: Receiver<(String, SocketAddr)>) {}
+
+#[tokio::main]
+async fn main() {
+    let mut stream = TcpStream::connect(SERVER).await.unwrap();
+    let (read, mut write) = stream.into_split();
+    println!("Connection with server established at {}, enter {} to exit chat", &mut read.peer_addr().unwrap(), QUIT.trim());
+
+    println!("Wait until you will be asked to enter your username");
+
+    let mut reader = BufReader::new(read);
+    let mut writer = BufWriter::new(write);
+    let (sender, mut receiver) = mpsc::channel::<String>(10);//10 wiadomości
+
+    let mut line = String::new();
+
+    //reader
+    tokio::spawn(async move {
+        loop {
+            line.clear();
+
+            let received = reader.read_line(&mut line).await;
+            if let Ok(_received_msg) = receiver.try_recv() { //wiadomość od drugiego taska o zakończeniu
+                println!("You have left the chat.");
+                break;
+            }
+            match received {
+                Ok(len) =>
+                    {
+                        if len == 0 {
+                            break;
+                        }
+                        println!("{}", line);
+                    }
+                Err(_) => {
+                    println!("Connection lost! type {} to quit", QUIT.trim());
+                }
+            }
+        }
+    });
+
+    //writer
+    let mut input = String::new();
+    loop {
+        input.clear();
+        let msg_send = stdin().read_line(&mut input); //user wpisuje wiadomość
+        match msg_send {
+            Ok(_) => {
+                if input == String::from(QUIT) {//user wpisał quit
+                    let quit_msg = input.clone();
+                    match sender.send(quit_msg).await { //wysyłanie waidomości o zakończeniu
+                        Ok(_) => {}
+                        Err(_) => {
+                            println!("Exiting ");
+                        }
+                    }
+                    break;
+                } else {
+                    let res = writer.write(input.as_bytes()).await; //wysyłanie do servera wiadomości
+                    match res {
+                        Ok(_) => { writer.flush().await.expect("Failed to flush buffer") }
+                        Err(_) => {
+                            println!("Connection lost! type {} to quit", QUIT.trim());
+                            break;
+                        }
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Error while reading input. ");
+            }
+        }
+    }
 }
--- chat_app/src/bin/server.rs
+++ chat_app/src/bin/server.rs
@@ -1,35 +1,41 @@
+use std::net::SocketAddr;
 use tokio::net::{TcpListener};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::sync::broadcast;
 use chat_app::types;
 
+
+async fn manageClient(){
+
+}
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, _rx) = broadcast::channel(10); //sending strings to max 10 ppl who can connect
+    let (sender, _rx) = broadcast::channel::<(String, SocketAddr) >(10); //sending strings to max 10 ppl who can connect
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
-        let tx = tx.clone();//klonowanie tx dla kazdego klienta
-        let mut rx = tx.subscribe();//nowy rx dla każdego klienta
-
+        let sender = sender.clone();//klonowanie tx dla kazdego klienta
+        let mut receiver = sender.subscribe();//nowy rx dla każdego klienta
+        println!("accepted player");
 
         tokio::spawn(async move { //spawnowanie taska obsługi klienta
             let (reader, mut writer) = socket.split(); //podział socketa na czytanie i pisanie
 
-            let mut reader = BufReader::new(reader);
+            let mut reader = BufReader::new(reader); //buffer czyta z socketa tcp od klienta
             let mut line = String::new();
 
             loop {
                 tokio::select! {
                     result = reader.read_line( & mut line) => { //pierwszy branch - otrzymanie wiadomosci do przeslania
-                        if result.unwrap() == 0 {
+                        if result.unwrap() == 0 { //koniec
                             break;
                         }
-                        tx.send((line.clone(), addr)).unwrap();
+                        sender.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
-                    result = rx.recv() => { //drugi branch - otrzymanie wiadomosci przez broadcast
+                    result = receiver.recv() => { //drugi branch - otrzymanie wiadomosci przez broadcast
                         let (msg, other_addr) = result.unwrap();
                         if addr != other_addr{
                         writer.write_all(msg.as_bytes()).await.unwrap();}
